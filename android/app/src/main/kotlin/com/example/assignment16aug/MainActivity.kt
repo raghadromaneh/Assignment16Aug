0b1a377b054f84dccad0f883c3e26c67de773c4a
raghadromaneh <raghadromani22@gmail.com>
2022-08-16 16:45:26 +0300

    last version

---
+++ android/app/src/main/kotlin/com/example/assignment16aug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment16aug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String name = "Raghad Roummaneh";
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(),
+        backgroundColor: Color.fromARGB(255, 220, 214, 214),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 48, 155, 155),
+          title: Text(
+            "My card",
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+          ),
+        ),
+        body: Card(
+          margin: EdgeInsets.all(5),
+          color: Color.fromARGB(255, 168, 89, 192),
+          elevation: 60,
+          child: Text(
+            "\n Welcome to ${name.toUpperCase()} card\n ",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w400,
+              color: Colors.purple[50],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
